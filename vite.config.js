116307a0146dbf6eff32d99aa93992b286ee5786
Tem235 <tem235@gmail.com>
2023-03-13 01:00:27 +0500

    Created app structure

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    root: 'src',
+    publicDir: "../public",
+    build: {
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                popup: resolve('src', 'popup.html'),
+                background: resolve('src', 'background.js'),
+                content: resolve('src', 'content.js'),
+            },
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
+    }
+})
