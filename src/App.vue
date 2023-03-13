5bf95871f8c1e3f59dd5e61e0ddd8e2726277040
Tem235 <tem235@gmail.com>
2023-03-13 13:57:52 +0500

    Added change bg function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 
 let url = ref('')
+let currentTab = ref('')
 
 var regExp = new RegExp(/(http(s)?:\/\/.)?(www\.)?(?:[-a-zA-Z0-9@:%_\+~#=]{1,63}\.)+[a-z]{2,6}\b((?:[-a-zA-Z0-9@:%_\+~#?&//="]+\.)*[-a-zA-Z0-9@:%_\+~#?&//="]+)?/);
 
@@ -26,14 +27,27 @@ function newTab() {
     chrome.tabs.create({ active: false, url: urlValidate.value, pinned: true })
     url.value = ''
 }
+
+function changeBg() {
+    chrome.tabs.sendMessage(currentTab.value.id, { message: "changeBg" });
+}
+
+onMounted(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function callback(tabs) {
+        currentTab.value = tabs[0];
+    });
+})
+
 </script>
 
 <template>
     <div class="popup">
         <div class="popup-form">
             <input v-model="url" type="text">
-            <button @click="newTab" :disabled="!urlValidate">Open new tab</button>
+            <button @click="newTab" :disabled="!urlValidate" class="btn">Open new tab</button>
         </div>
+        <button @click="changeBg" v-if="currentTab.url == 'https://www.google.com/'" class="btn btn--change">Change Google
+            background</button>
     </div>
 </template>
 
@@ -49,7 +63,11 @@ body {
     background: #a9dbf5;
 }
 
-.popup-form {}
+.popup-form {
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 10px;
+}
 
 .popup input {
     width: 120px;
@@ -63,13 +81,6 @@ body {
     border: 2px solid #185675;
 }
 
-.popup button {
-    background: #43809f;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-}
-
 .popup button:hover {
     background: #185675;
     cursor: pointer;
@@ -79,5 +90,16 @@ body {
 .popup button:disabled {
     background: #5b5b5b;
     cursor: not-allowed;
+}
+
+.btn {
+    background: #43809f;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+}
+
+.btn--change {
+    width: 100%;
 }
 </style>
--- src/content.js
+++ src/content.js
@@ -1,1 +1,10 @@
-alert("Hello from Content script")
+chrome.runtime.onMessage.addListener((msg, _sender, _response) => {
+    if (msg.message === 'changeBg') {
+        if (window.getComputedStyle(document.body, null).getPropertyValue('background-color') === 'rgb(255, 255, 255)') {
+            document.body.style.backgroundColor = "#175417"
+        } else {
+            document.body.style.backgroundColor = "#ffffff"
+        }
+    }
+});
+
